0fac2093f02ae16778e732481e526b64bdf8fae0
madebyTimo <t.schnaible@madebytimo.de>
2022-06-22 23:59:18 +0200

    add HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <div class="col-4 position-relative">
+      <button class="btn btn-primary position-absolute top-50 start-50 translate-middle">Start</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
