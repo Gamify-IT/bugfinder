4511ee497d0129742ccd1e126b8dcff3078baf9e
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-06-25 13:31:19 +0200

    added check if already submitted in game logic

--- tests/unit/bugfindergame.spec.ts
+++ tests/unit/bugfindergame.spec.ts
@@ -50,6 +50,17 @@ describe('bugfindergame.ts', () => {
     const result = bugfindergame.submitWrongCode(4);
     expect(result).toBe(false);
   });
+  it('Succed to get hasSubmitted false when not submitted', () => {
+    expect(bugfindergame.hasSubmitted()).toBe(false);
+  });
+  it('Succed to get hasSubmitted true when already submitted', () => {
+    bugfindergame.submitWrongCode(1);
+    expect(bugfindergame.hasSubmitted()).toBe(true);
+  });
+  it('Fails when submits twice', () => {
+    bugfindergame.submitWrongCode(1);
+    expect(() => bugfindergame.submitWrongCode(1)).toThrow();
+  });
   it('Succeed when want to go to next code and finished current', () => {
     const result = bugfindergame.submitWrongCode(1);
     bugfindergame.nextCode();
