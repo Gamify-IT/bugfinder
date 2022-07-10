02bdc7b9eaef59cd42bf98baeffaf74274217e72
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-07-10 20:55:48 +0200

    create test for new code props and remove test remove code after select

--- tests/unit/CodeBox.spec.ts
+++ tests/unit/CodeBox.spec.ts
@@ -26,6 +26,30 @@ function exampleCode(): Code {
   ]);
 }
 
+function exampleCode2(): Code {
+  return new Code(1, [
+    new Word(1, WordType.SPACE),
+    new Word(2, 'prvte'),
+    new Word(3, WordType.SPACE),
+    new Word(4, 'void'),
+    new Word(5, WordType.SPACE),
+    new Word(6, 'sayBye()'),
+    new Word(7, WordType.SPACE),
+    new Word(8, '{'),
+    new Word(9, WordType.NEWLINE),
+    new Word(10, WordType.TAB),
+    new Word(11, WordType.SPACE),
+    new Word(12, 'System.out.println("BYE THERE!")'),
+    new Word(13, WordType.NEWLINE),
+    new Word(14, WordType.TAB),
+    new Word(15, WordType.SPACE),
+    new Word(16, 'System.out.println("REALLY BYE THERE!")'),
+    new Word(17, WordType.NEWLINE),
+    new Word(18, WordType.SPACE),
+    new Word(19, '}'),
+  ]);
+}
+
 describe('CodeBox.vue', () => {
   let wrapper: VueWrapper;
   let code: ICode;
@@ -53,6 +77,19 @@ describe('CodeBox.vue', () => {
 
     const spaces = wrapper.findAll('.code-space');
     expect(spaces.length).toBe(code.words.filter((word) => word.word == WordType.SPACE).length);
+  });
+  test('CodeBox shows code correct after new props rendered', async () => {
+    const newCode = exampleCode2();
+    await wrapper.setProps({ code: newCode });
+
+    const lines = wrapper.findAll('.code-line');
+    expect(lines.length).toBe(newCode.words.filter((word) => word.word == WordType.NEWLINE).length + 1);
+
+    const words = wrapper.findAll('.code-word');
+    expect(words.length).toBe(newCode.words.filter((word) => word.word != WordType.TAB && word.word != WordType.NEWLINE).length);
+
+    const spaces = wrapper.findAll('.code-space');
+    expect(spaces.length).toBe(newCode.words.filter((word) => word.word == WordType.SPACE).length);
   });
   test('CodeBox shows no feedback when there is none feedback provided', () => {
     const rightCodes = wrapper.findAll('.right-code');
@@ -93,7 +130,7 @@ describe('CodeBox.vue', () => {
 
     expect(wrapper.find('.btn-success').exists()).toBe(false);
   })
-  test('Select code word', async () => {
+  test('Select and remove code word', async () => {
     const selectedWord = 2;
     const targetWordButton = wrapper.find('#word-' + selectedWord);
     expect(targetWordButton.exists()).toBe(true);
@@ -114,9 +151,15 @@ describe('CodeBox.vue', () => {
 
     await wrapper.vm.$nextTick();
 
-    const selectedCodes = wrapper.findAll('.selected-code');
+    let selectedCodes = wrapper.findAll('.selected-code');
     expect(selectedCodes.length).toBe(1);
     expect(selectedCodes[0].element.id).toBe('word-' + selectedWord);
+
+    targetWordButton.trigger('click');
+    await wrapper.vm.$nextTick();
+
+    selectedCodes = wrapper.findAll('.selected-code');
+    expect(selectedCodes.length).toBe(0);
 
   });
 });
