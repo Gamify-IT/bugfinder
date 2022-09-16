8c96b4476e7cdc5e32dd3a9d674a2dbed10509b4
Ilijaz Mehmedovic <mehmedovic.ilijaz@gmail.com>
2022-09-16 18:28:21 +0200

    add warn message when no configuration is specified in the url

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,21 @@
 const emit = defineEmits<{
   (e: 'startGame'): void;
 }>();
+
+const configuration = window.location.pathname.split('/').pop();
 </script>
 
 <template>
   <main>
-    <div>
+    <h1>test</h1>
+    <h2>{{ configuration }}</h2>
+    <div v-if="configuration?.length !== 0">
       <button class="btn btn-primary position-absolute top-50 start-50 translate-middle" @click="emit('startGame')">Start</button>
+    </div>
+    <div v-if="configuration?.length === 0">
+      <div class="position-absolute top-50 start-50 translate-middle alert alert-danger">
+        No configuration specified in the url
+      </div>
     </div>
   </main>
 </template>
