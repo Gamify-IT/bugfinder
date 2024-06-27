065e68d52d4c0f51f8f53de3a4c02fe27f100a21
Duyguoe <duyguoezden96@gmail.com>
2024-06-27 18:23:32 +0200

    Update FinishView.vue

--- src/views/FinishView.vue
+++ src/views/FinishView.vue
@@ -1,43 +1,14 @@
 <script setup lang="ts">
-
-import { ref, onMounted } from 'vue';
-import { BugFinderGame } from '@/services/bugfindergame';
-
-const score = ref(0);
-const rewards = ref(0);
-
-let gameInstance: BugFinderGame | null = null;
-
-
 function closeGame() {
   window.parent.postMessage('CLOSE ME');
 }
-
-onMounted(async () => {
-  try {
-
-    gameInstance = new BugFinderGame('configurationId');
-
-    score.value = gameInstance.getScore();
-    rewards.value = gameInstance.getRewards();
-
-  } catch (error) {
-    console.error('Error initializing BugFinderGame:', error);
-  }
-});
-
 </script>
 
 <template>
   <main>
     <div class="text-center position-absolute top-50 start-50 translate-middle">
-      <div v-if="score == null ">
-      <h2>You finished the Game!Your Score: {{ score }}! Your Rewards: {{ rewards }}!</h2>
+      <h2>You finished the Game!</h2>
       <b-button variant="danger" id="close-button" v-on:click="closeGame"> Back to game </b-button>
-    </div>
-    <div v-else>
-      Loading...
-    </div>
     </div>
   </main>
 </template>
