bfce403cd03d42b1533247a36fd31f3125e297e2
Duyguoe <duyguoezden96@gmail.com>
2024-06-27 20:35:49 +0200

    Update FinishView.vue

--- src/views/FinishView.vue
+++ src/views/FinishView.vue
@@ -1,22 +1,14 @@
 <script setup lang="ts">
-import mitt, { Emitter } from 'mitt';
+import mitt from 'mitt';
 import { ref } from 'vue';
 
-
+const emitter = mitt();
+let score = ref(0);
 let rewards = ref(0);
 
-type Events = {
-  RewardsAndScores: { score: number };
-  // Weitere Ereignisse hier...
-};
 
-const emitter: Emitter<Events> = mitt<Events>();
-const score = ref(0);
+emitter.on('RewardsAndScores',  score => console.log('RewardsAndScores', score) )
 
-emitter.on('RewardsAndScores', (data: { score: number }) => {
-  console.log('RewardsAndScores', data.score);
-  score.value = data.score;
-});
 function closeGame() {
   window.parent.postMessage('CLOSE ME');
 
@@ -26,7 +18,7 @@ function closeGame() {
 <template>
   <main>
     <div class="text-center position-absolute top-50 start-50 translate-middle">
-      <h2>You finished the Game! Score: {{ score }}</h2>
+      <h2>You finished the Game!! Score: {{ score }}</h2>
       <b-button variant="danger" id="close-button" v-on:click="closeGame"> Back to game </b-button>
     </div>
   </main>
