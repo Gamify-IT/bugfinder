95eb659d1f54151bc89e268b39dbe20a6ac57da6
Duyguoe <duyguoezden96@gmail.com>
2024-07-12 16:11:52 +0200

    Updated design of overview window

--- src/views/FinishView.vue
+++ src/views/FinishView.vue
@@ -1,19 +1,88 @@
 <script setup lang="ts">
 import { useStore } from 'vuex';
-
+import { computed } from 'vue';
 const store = useStore();
 
 function closeGame() {
   window.parent.postMessage('CLOSE ME');
 }
 
+const message = computed(() => {
+  const score = store.state.score;
+  if (score >= 0 && score <= 40) {
+    return "Don't give up! You've got this!";
+  } else if (score >= 50 && score <= 70) {
+    return "Good job, keep it up!!";
+  } else if (score >= 80 && score <= 100) {
+    return "Wooow, congratulations ! Really nice!";
+  } else {
+    return "";
+  }
+});
+
+
 </script>
 
+
 <template>
-  <main>
+  <main class="background-image">
     <div class="text-center position-absolute top-50 start-50 translate-middle">
-      <h2>You finished the Game!! You've gained {{ store.state.score }} scores and {{ store.state.rewards }} coins !! </h2>
+      <h2 class="score-coins">
+        You finished the Game! You've gained
+        <span class="gold-outlined-text">{{ store.state.score }}</span> <span class="gold-outlined-text">scores</span> and
+        <span class="gold-outlined-text">{{ store.state.rewards }}</span> <span class="gold-outlined-text">coins</span> !!
+      </h2>
+      <p class="message">{{ message }}</p>
       <b-button variant="danger" id="close-button" v-on:click="closeGame"> Back to game </b-button>
     </div>
   </main>
 </template>
+
+
+
+<style scoped>
+.background-image {
+  background-image: url('@/assets/background.webp');
+  background-size: cover; 
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+
+.score-coins {
+  color: white;
+  text-align: center;
+  font-size: 60px;
+}
+
+.gold-outlined-text {
+  color: gold;
+  text-shadow:
+    -1px -1px 0 gold,
+    1px -1px 0 gold,
+    -1px 1px 0 gold,
+    1px 1px 0 gold;
+}
+
+.message {
+  font-size: 53px;
+  color: white;
+  text-align: center;
+}
+
+.gold-outlined-text.message {
+  color: gold;
+  text-shadow:
+    -1px -1px 0 gold,
+    1px -1px 0 gold,
+    -1px 1px 0 gold,
+    1px 1px 0 gold;
+}
+
+#close-button {
+  color: white;
+  margin-top: 20px;
+}
+</style>
